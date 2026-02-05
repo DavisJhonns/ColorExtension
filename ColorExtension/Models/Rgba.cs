d70186335851fb659ad7438af55161d18eb9d2e1
Davis Jhonns <davis.jhonns@gmail.com>
2026-02-05 09:43:16 +0100

    dev: created Rgba model

---
+++ ColorExtension/Models/Rgba.cs
@@ -0,0 +1,15 @@
+﻿namespace ColorExtension.Models;
+
+/// <summary>
+/// Represents a color using RGBA (Red, Green, Blue, Alpha) components.
+/// </summary>
+/// <param name="A">The alpha (opacity) component.</param>
+/// <param name="R">The red component.</param>
+/// <param name="G">The green component.</param>
+/// <param name="B">The blue component.</param>
+public readonly record struct Rgba(
+    byte A,
+    byte R,
+    byte G,
+    byte B
+);
